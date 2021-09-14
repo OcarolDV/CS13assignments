f4b733c04e33a61e420953b5320c6ebf3fb3897e
Dar Vince <ocaroldar@gmail.com>
2021-09-14 13:23:19 -0700

    Got initial files from the assignment page

---
+++ Payroll-2.java
@@ -0,0 +1,67 @@
+public class Payroll
+{
+   //your code
+   
+   
+   
+   
+   
+   
+   
+   
+   
+}
+//once you implement the Payroll class, uncommnet the code in the main method and run your program. 
+class PayrollDriver
+{
+  public static void main(String[] args)
+  {
+   
+  /* uncommnet  the provided code below
+    System.out.println("Cretaing payroll objects");
+    Payroll p1 = new Payroll("Alex Martinez" ,"123456", 25, 20);
+    Payroll p2 = new Payroll("Ali Santos" ,"986747", 125, 45);
+    Payroll p3 = new Payroll(" Jose Busta" ,"45678", 55, 30);
+    System.out.println("testing the toString method");
+    
+    System.out.println(p1);
+    System.out.println("Salary is : " + p1.getPay());  //calling the getPay method
+    System.out.println("\n*******************");
+    System.out.println(p2);
+    System.out.println("Salary is : "+ p2.getPay());
+    System.out.println("\n*******************");
+    System.out.println(p3);
+    System.out.println("Salary is : "+ p3.getPay());
+    System.out.println("\n*******************");
+    System.out.println("\nTesting the setter methods");
+    System.out.println("The hourly pay of " + p1.getName()  + " is being chnaged");
+    p1.setHoursWorked(80);  // changing the hours worked for the object p1
+    System.out.println(p1);   */ 
+    
+    
+      
+    
+    
+    //***********************************************************************************
+    
+    
+    /*        Your turn , you need to implement code for each commnet  below       */
+     
+    //1. create two objects of the payroll class 
+    
+    //2. display the objects on the screen by calling the toString method
+    
+    //2. display the salary of each person by calling the getPay method
+    
+    //change the hourlyRate of the objects you created
+    
+    //display the objects again to see the changes you made by calling the toString method
+    
+    //change the hoursworked for the objects you created by calling the setter methods
+    
+    //display your objects again to see the changes you made
+    
+    //add anyother code you want
+    
+  }
+}
